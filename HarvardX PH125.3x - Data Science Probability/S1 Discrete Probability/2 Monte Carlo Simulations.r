891c51eaa7f72aeb1bd95a1a93317dd9b09c629e
Azmine Toushik Wasi <azmine.wasi@gmail.com>
2021-06-01 20:55:24 +0600

    DS Prob

---
+++ HarvardX PH125.3x - Data Science Probability/S1 Discrete Probability/2 Monte Carlo Simulations.r
@@ -0,0 +1,9 @@
+beads <- rep(c("red", "blue"), times = c(2,3))    # create an urn with 2 red, 3 blue
+beads    # view beads object
+sample(beads, 1)    # sample 1 bead at random
+
+B <- 10000    # number of times to draw 1 bead
+events <- replicate(B, sample(beads, 1))    # draw 1 bead, B times
+tab <- table(events)    # make a table of outcome counts
+tab    # view count table
+prop.table(tab)    # view table of outcome proportions

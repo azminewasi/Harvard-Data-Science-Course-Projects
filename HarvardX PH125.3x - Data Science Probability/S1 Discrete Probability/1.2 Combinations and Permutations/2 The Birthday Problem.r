04b564e2558a51fe26f4077b1a15c72ae3b26aa3
Azmine Toushik Wasi <azmine.wasi@gmail.com>
2021-06-02 23:46:06 +0600

    Section 1 completed

---
+++ HarvardX PH125.3x - Data Science Probability/S1 Discrete Probability/1.2 Combinations and Permutations/2 The Birthday Problem.r
@@ -0,0 +1,13 @@
+#Code: The birthday problem
+# checking for duplicated bdays in one 50 person group
+n <- 50
+bdays <- sample(1:365, n, replace = TRUE)    # generate n random birthdays
+any(duplicated(bdays))    # check if any birthdays are duplicated
+
+# Monte Carlo simulation with B=10000 replicates
+B <- 10000
+results <- replicate(B, {    # returns vector of B logical values
+  bdays <- sample(1:365, n, replace = TRUE)
+  any(duplicated(bdays))
+})
+mean(results)    # calculates proportion of groups with duplicated bdays
---
+++ HarvardX PH125.3x - Data Science Probability/S1 Discrete Probability/1.2 Combinations and Permutations/4 How Many Monte Carlo Experiments are Enough.r
@@ -0,0 +1,11 @@
+B <- 10^seq(1, 5, len = 100)    # defines vector of many B values
+compute_prob <- function(B, n = 22){    # function to run Monte Carlo simulation with each B
+  same_day <- replicate(B, {
+    bdays <- sample(1:365, n, replace = TRUE)
+    any(duplicated(bdays))
+  })
+  mean(same_day)
+}
+
+prob <- sapply(B, compute_prob)    # apply compute_prob to many values of B
+plot(log10(B), prob, type = "l")    # plot a line graph of estimates 
